8358eb840608fb0b57fe5620b6af9320c6b808f0
Bob Le <bob.hoangle@gmail.com>
2019-06-21 15:53:12 -0500

    ranking complete chess

--- src/App.js
+++ src/App.js
@@ -1,26 +1,251 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Dropzone from 'react-dropzone'
+import {Container, Paper, Button} from '@material-ui/core/';
+import MaterialTable from 'material-table'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: null,
+            headRows: ["USCF ID", "Name", "Grade", "Gender", "Last Event", ""],
+            columns: [
+                {title: 'USCF ID', field: 'USCF ID', editable: 'never'},
+                {
+                    title: 'Name',
+                    field: 'Name',
+                    render: rowData => <a href={`http://www.uschess.org/msa/MbrDtlMain.php?${rowData["USCF ID"]}`} target="_blank">{rowData["Name"]}</a>,
+                },
+                {title: 'Grade', field: 'Grade', type: 'numeric'},
+                {title: 'Gender', field: 'Gender'},
+                {title: 'Last Event', field: 'Last Event'},
+                {title: 'Regular Rating', field: 'Regular Rating', type: 'numeric', defaultSort: 'desc'},
+            ],
+            players: [],
+            generated: false
+        };
+        this.createTables = this.createTables.bind(this);
+        this.dropFile = this.dropFile.bind(this);
+        this.saveData = this.saveData.bind(this);
+        this.reloadData = this.reloadData.bind(this);
+        this.createTables = this.createTables.bind(this);
+    }
+
+    dropFile(file) {
+        let fileReader;
+        let self = this;
+        const handleFileRead = (e) => {
+            const content = fileReader.result;
+            this.setState({
+                data: content
+            });
+            self.parsePlayers();
+        };
+        const handleFileChose = (file) => {
+            fileReader = new FileReader();
+            fileReader.onloadend = handleFileRead;
+            fileReader.readAsText(file);
+        };
+        handleFileChose(file[0]);
+    }
+
+    parsePlayers() {
+        let {data} = this.state;
+        let playersList = [];
+        let players = [];
+        let lines = data.split('\n');
+        let i, j, temp, chunk = 15;
+        for (i = 0, j = lines.length; i < j; i += chunk) {
+            temp = lines.slice(i, i + chunk);
+            if (temp !== [""]) playersList.push(temp);
+        }
+        for (i = 0; i < playersList.length; i++) {
+            players.push(this.getPlayer(playersList[i]));
+        }
+        this.setState({players})
+    }
+
+    getPlayer(data) {
+        let i;
+        let player = {};
+        const convertToNum = (i) => {
+            let res = i;
+            if (res.indexOf('*') > 0) res = res.substring(0, res.indexOf('*'));
+            if (res.indexOf('/') > 0) res = res.substring(0, res.indexOf('/'));
+            return parseInt(res, 10);
+        };
+        for (i = 0; i < data.length - 1; i++) {
+            let lines = data[i].split(':');
+            if (lines[0] === "Regular Rating") {
+                let num = convertToNum(lines[1]) || 0;
+                player[lines[0]] = num;
+            } else {
+                player[lines[0]] = lines[1]
+            }
+        }
+        return player;
+    }
+
+    createTables() {
+        let {players} = this.state;
+        if (players) {
+            let topFemales, topK12, topK9, topK5;
+            topFemales = players.filter(player => player["Gender"] === "Female");
+            topK12 = players.filter(player => player["Grade"] < 13 && player["Grade"] > 0);
+            topK9 = players.filter(player => player["Grade"] < 10 && player["Grade"] > 0);
+            topK5 = players.filter(player => player["Grade"] < 6 && player["Grade"] > 0);
+            this.setState({
+                generated: true,
+                topFemales: topFemales,
+                topK12: topK12,
+                topK9: topK9,
+                topK5: topK5,
+            })
+        }
+    }
+
+    saveData() {
+        let {players} = this.state;
+        localStorage.setItem('ccPlayers', JSON.stringify(players));
+    }
+
+    reloadData() {
+        let res;
+        let tmp = localStorage.getItem("ccPlayers");
+        if (tmp) {
+            res = JSON.parse(tmp)
+        }
+        this.setState({
+            players: res,
+            generated: false
+        })
+    }
+
+    render() {
+        let {columns, players, generated, topFemales, topK12, topK9, topK5} = this.state;
+        const editTable =
+            <Paper>
+                <MaterialTable
+                    title="Edit Data"
+                    columns={columns}
+                    data={players}
+                    options={{pageSize: 10}}
+                    editable={{
+                        onRowAdd: newData =>
+                            new Promise((resolve, reject) => {
+                                setTimeout(() => {
+                                    players.push(newData);
+                                    this.setState({players}, () => resolve());
+                                    resolve()
+                                }, 1000)
+                            }),
+                        onRowUpdate: (newData, oldData) =>
+                            new Promise((resolve, reject) => {
+                                setTimeout(() => {
+                                    const players = this.state.players;
+                                    const index = players.indexOf(oldData);
+                                    players[index] = newData;
+                                    this.setState({players}, () => resolve());
+                                    resolve()
+                                }, 1000)
+                            }),
+                        onRowDelete: oldData =>
+                            new Promise((resolve, reject) => {
+                                setTimeout(() => {
+                                    const index = players.indexOf(oldData);
+                                    players.splice(index, 1);
+                                    this.setState({players}, () => resolve());
+                                    resolve()
+                                }, 1000)
+                            }),
+                    }}
+                />
+            </Paper>;
+        const generatedTables =
+            <Paper>
+                <MaterialTable
+                    title="Overall Ranking"
+                    columns={columns}
+                    data={players}
+                    options={{
+                        search: false,
+                        pageSize: 10
+                    }}
+                />
+                <MaterialTable
+                    title="Top Females"
+                    columns={columns}
+                    data={topFemales}
+                    options={{
+                        search: false,
+                        pageSize: 10
+                    }}
+                />
+                <MaterialTable
+                    title="Top K-12"
+                    columns={columns}
+                    data={topK12}
+                    options={{
+                        search: false,
+                        pageSize: 10
+                    }}
+                />
+                <MaterialTable
+                    title="Top K-9"
+                    columns={columns}
+                    data={topK9}
+                    options={{
+                        search: false,
+                        pageSize: 10
+                    }}
+                />
+                <MaterialTable
+                    title="Top K-5"
+                    columns={columns}
+                    data={topK5}
+                    options={{
+                        search: false,
+                        pageSize: 10
+                    }}
+                />
+            </Paper>;
+        return (
+            <div className="App">
+                <Container className="drop-zone">
+                    <Dropzone
+                        accept={".txt"}
+                        multiple={false}
+                        onDrop={acceptedFiles => this.dropFile(acceptedFiles)}
+                    >
+                        {({getRootProps, getInputProps}) => (
+                            <section>
+                                <div {...getRootProps()}>
+                                    <input {...getInputProps()} />
+                                    <p>Drag & drop some files here, or click to select files</p>
+                                </div>
+                            </section>
+                        )}
+                    </Dropzone>
+                    <Button onClick={this.saveData} variant="contained" className="cc-button">
+                        Save Data
+                    </Button>
+                    <Button onClick={this.reloadData} variant="contained" color="primary" className="cc-button">
+                        Reload Data
+                    </Button>
+                    <Button onClick={this.createTables} variant="contained" color="secondary" className="cc-button">
+                        Generate Tables
+                    </Button>
+                </Container>
+                {players.length > 0 ?
+                    <Container>
+                        {generated ? generatedTables : editTable}
+                    </Container>
+                    : null
+                }
+            </div>
+        )
+    }
 }
 
 export default App;
